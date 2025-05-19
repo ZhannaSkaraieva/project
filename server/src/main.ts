fa08cd312c107066a969dcf9a479c7f4a3ca49c8
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-05-19 13:43:43 +0300

    CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors(); //Cross-origin resource sharing (CORS) — это механизм, который позволяет запрашивать ресурсы из другого домена. 
+  //Чтобы включить CORS, вызовите enableCors()метод для объекта приложения Nest.
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
