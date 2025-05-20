4f50d6754451a690271233ddbec3af6910455246
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-05-20 22:47:17 +0300

    prisma servis

--- server/src/prisma/prisma.module.ts
+++ server/src/prisma/prisma.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { MongoPrismaService } from './mongo-prisma/mongo-prisma.service';
+import { PostgresPrismaService } from './postgres-prisma/postgres-prisma.service';
 
 @Module({
-    providers: [PrismaService],
+    providers: [PrismaService, PostgresPrismaService, MongoPrismaService],
   exports: [PrismaService],
 })
 export class PrismaModule {}
+
